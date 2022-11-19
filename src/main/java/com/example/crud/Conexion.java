4a6278291df2b8eabc5da24cbf29d82c6caac848
Sebastianfacundo <Sebastianvera38@gmail.com>
2022-11-19 13:36:13 -0300

    modificacion nombre base

--- src/main/java/com/example/crud/Conexion.java
+++ src/main/java/com/example/crud/Conexion.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 
 public class Conexion {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/empresa?autoReconnet=true&useSSL=false";
+    private static final String URL = "jdbc:mysql://localhost:3306/nombre_base?autoReconnet=true&useSSL=false";
     private static final String USUARIO = "root";
     private static final String PASS = "";
     private static Connection con = null;
